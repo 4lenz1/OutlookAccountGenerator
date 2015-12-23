62339a4807e66bd06a205fe8f1ea679bb3513cb3
4lenz1 <pingfallenzone@gmail.com>
2015-12-23 18:09:27 +0800

    bug fix
    datepicker changed
    
    set today to default.

---
+++ OutlookAccountGenerator/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace OutlookAccountGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputAmount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inputApplicant = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputAmount
+            // 
+            this.inputAmount.Location = new System.Drawing.Point(147, 82);
+            this.inputAmount.Name = "inputAmount";
+            this.inputAmount.Size = new System.Drawing.Size(173, 20);
+            this.inputAmount.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Amount";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(66, 194);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(254, 40);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Applicant";
+            // 
+            // inputApplicant
+            // 
+            this.inputApplicant.Location = new System.Drawing.Point(147, 149);
+            this.inputApplicant.Name = "inputApplicant";
+            this.inputApplicant.Size = new System.Drawing.Size(173, 20);
+            this.inputApplicant.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 10;
+            // 
+            // datePicker
+            // 
+            this.datePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datePicker.Location = new System.Drawing.Point(87, 28);
+            this.datePicker.MinDate = new System.DateTime(2015, 11, 27, 0, 0, 0, 0);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(200, 20);
+            this.datePicker.TabIndex = 1;
+            this.datePicker.Value = new System.DateTime(2015, 12, 23, 0, 0, 0, 0);
+            this.datePicker.ValueChanged += new System.EventHandler(this.datePicker_ValueChanged);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(69, 248);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(251, 20);
+            this.txtFileName.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 280);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.inputApplicant);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.inputAmount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox inputAmount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox inputApplicant;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.TextBox txtFileName;
+    }
+}
+
---
+++ OutlookAccountGenerator/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace OutlookAccountGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            datePicker.Value = DateTime.Now;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            //string filename = "Azure_Pass_Account" +/* datePicker.ToString() + */inputApplicant.Text;
+            Excel.Application excel = new Excel.Application();
+            excel.Visible = true;
+            Excel.Workbook wb = excel.Workbooks.Add(1);
+            Excel.Worksheet sh = wb.Sheets.Add();
+            datePicker.Format = DateTimePickerFormat.Custom;
+            datePicker.CustomFormat = "yyyyMMdd";
+          
+            sh.Name = "Azure Account";
+
+            sh.Cells[1, "A"].Value2 = "Index";
+            sh.Cells[1, "B"].Value2 = "Account";
+            sh.Cells[1, "C"].Value2 = "Password";
+            sh.Cells[1, "D"].Value2 = "Applicant";
+
+
+            for(int index = 1; index <= Int32.Parse( inputAmount.Text); index   ++)
+            {
+                sh.Cells[index + 1 ,"A"].Value2 = index;
+                sh.Cells[index + 1, "B"].Value2 = "MS" + datePicker.Text + index.ToString("00") + "@outlook.com";
+                sh.Cells[index + 1, "C"].Value2 = "MS" + datePicker.Text;
+                sh.Cells[index + 1, "D"].Value2 = inputApplicant.Text;
+            }
+
+            txtFileName.Text = "Azure_Pass_Account_" + datePicker.Text + "_" + inputApplicant.Text + "auto_generate";
+
+            wb.Close(true);
+            excel.Quit();
+
+        }
+
+        private void datePicker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
